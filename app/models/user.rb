1d70126bab1be0f770c4993cb59a83260607d349
alexbuchan <alex.buchan@telefonica.net>
2020-06-02 16:53:36 +0200

    Update User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
+  validates :username, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
-
-  validates_length_of :username, :minimum => 3
-  validates_length_of :password, :minimum => 6
 end
