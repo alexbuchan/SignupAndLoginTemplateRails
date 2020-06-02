75712630690df10ac3fb6b49a533770368dfe482
alexbuchan <alex.buchan@telefonica.net>
2020-06-02 16:53:06 +0200

    Add api call to fetch fake contacts. Add more data to jwt for login.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,3 +1,5 @@
+require 'rest-client'
+
 class AuthenticationController < ApplicationController
   before_action :authorize_request, except: :login
 
@@ -5,16 +7,31 @@ class AuthenticationController < ApplicationController
   def login
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
+      token = token = JsonWebToken::JsonWebTokenHelper.encode({
+        user: {
+          user_id: @user.id,
+          username: @user.username,
+          email: @user.email
+        },
+        auth: true,
+        message: 'User registered'
+      })
+
       time = Time.now + 24.hours.to_i
       render json: { 
         token: token, 
         exp: time.strftime("%m-%d-%Y %H:%M"),
-        username: @user.username 
       }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
+  end
+
+  # GET /auth/contacts
+  def contacts
+    response = RestClient.get('https://jsonplaceholder.typicode.com/users')
+    body = JSON.parse(response.body)
+    render json: body, status: :ok
   end
 
   private
