31bfaabe434e8ed5b5b11c526eb61a6581ef1716
alexbuchan <alex.buchan@telefonica.net>
2020-06-10 07:28:54 +0200

    Refactor UsersController specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,24 +3,26 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
+  let(:response_body) { JSON.parse(response.body) }
+
   describe 'POST create' do
+    before :each do
+      post :create, params: { user: attr }
+    end
+
     context 'when user is saved correctly' do
-      let(:jwt_response) { JsonWebToken::JsonWebTokenHelper.decode(JSON.parse(response.body)['token']) }
       let(:attr) { { username: 'username', email: 'user@email.com', password: 'password' } }
 
       it 'sends json response with jwt' do
-        post :create, params: { user: attr }
-        expect(jwt_response[:user][:id]).to eq(1)
+        expect(response_body).to have_key('token')
       end
     end
 
     context 'when user is NOT saved correctly' do
-      let(:error_response) { JSON.parse(response.body) }
       let(:attr) { { username: 'us', email: 'user@email', password: 'pass' } }
 
       it 'sends json error response' do
-        post :create, params: { user: attr }
-        expect(error_response['error']).to eq([
+        expect(response_body['error']).to eq([
           'Username is too short (minimum is 3 characters)',
           'Email is invalid',
           'Password is too short (minimum is 6 characters)'
@@ -32,18 +34,21 @@ describe UsersController, type: :controller do
   describe 'PUT update' do
     let(:attr) { { username: 'username', email: 'user@email.com', password: 'password' } }
     let(:user) { User.create(attr) }
+    let(:token) { JsonWebToken::JsonWebTokenHelper.encode(new_attr) }
+    let(:headers) { { 'Authorization': "Bearer #{token}" } }
+
+    before :each do
+      request.headers['Authorization'] = headers[:Authorization]
+      put :update, params: { id: user.to_param.to_i, user: new_attr }
+    end
 
     context 'when user is updated correctly' do
       let(:jwt_response) { JsonWebToken::JsonWebTokenHelper.decode(JSON.parse(response.body)['token']) }
-      let(:new_attr) { { id: 2, username: 'new username', email: 'new_user@email.com', password: 'password' } }
-      let(:token) { JsonWebToken::JsonWebTokenHelper.encode(new_attr) }
-      let(:headers) { { 'Authorization': "Bearer #{token}" } }
+      let(:new_attr) do
+        { id: user.to_param.to_i, username: 'new username', email: 'new_user@email.com', password: 'password' }
+      end
 
       it 'sends json response with jwt' do
-        request.headers['Authorization'] = headers[:Authorization]
-        put :update, params: { id: user.to_param.to_i, user: new_attr }
-
-        user.reload
         jwt_response[:user].to_h.symbolize_keys.each do |key, param|
           expect(param).to eq(new_attr[key])
         end
@@ -51,16 +56,10 @@ describe UsersController, type: :controller do
     end
 
     context 'when user is NOT updated correctly' do
-      let(:error_response) { JSON.parse(response.body) }
-      let(:new_attr) { { id: 3, username: 'us', email: 'new_user@email', password: 'pass' } }
-      let(:token) { JsonWebToken::JsonWebTokenHelper.encode(new_attr) }
-      let(:headers) { { 'Authorization': "Bearer #{token}" } }
+      let(:new_attr) { { id: user.to_param.to_i, username: 'us', email: 'new_user@email', password: 'pass' } }
 
       it 'sends json error response' do
-        request.headers['Authorization'] = headers[:Authorization]
-        put :update, params: { id: user.to_param.to_i, user: new_attr }
-
-        expect(error_response['error']).to eq([
+        expect(response_body['error']).to eq([
           'Username is too short (minimum is 3 characters)',
           'Email is invalid',
           'Password is too short (minimum is 6 characters)'
